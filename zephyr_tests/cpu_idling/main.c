a2bfa0924937ec7ff8427f8169c31e4338a16cbf
fasttrack611 <130031515+fasttrack611@users.noreply.github.com>
2025-07-19 14:19:11 +0530

    Update main.c

--- zephyr_tests/cpu_idling/main.c
+++ zephyr_tests/cpu_idling/main.c
@@ -1,20 +1,47 @@
-/*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
 
-#include <zephyr.h>
-#include <sys/printk.h>
+#define STACK_SIZE 512
+#define THREAD_PRIORITY 5
+
+K_THREAD_STACK_DEFINE(thread1_stack, STACK_SIZE);
+K_THREAD_STACK_DEFINE(thread2_stack, STACK_SIZE);
+
+K_THREAD_STACK_DEFINE(thread1_stack, STACK_SIZE);
+K_THREAD_STACK_DEFINE(thread2_stack, STACK_SIZE);
+
+struct k_thread thread1_data;
+struct k_thread thread2_data;
+
+void thread1(void *arg1, void *arg2, void *arg3)
+{
+    while (1) {
+        printk("Thread 1: Going to sleep for 1 second...\n");
+        k_cpu_idle();
+        printk("Thread 1: Woke up!\n");
+        k_sleep(K_SECONDS(1));
+    }
+}
+
+void thread2(void *arg1, void *arg2, void *arg3)
+{
+    while (1) {
+        printk("Thread 2: Going to sleep for 2 seconds...\n");
+        k_cpu_idle();
+        printk("Thread 2: Woke up!\n");
+        k_sleep(K_SECONDS(2));
+    }
+}
 
 void main(void)
 {
-	printk("CPU Idling sample started\n");
+    printk("CPU Idling sample with 2 threads started\n");
 
-	while (1) {
-		printk("Going to sleep for 1 second...\n");
-		k_cpu_idle();
-		printk("Woke up!\n");
-		k_sleep(K_SECONDS(1));
-	}
+    k_thread_create(&thread1_data, thread1_stack, STACK_SIZE,
+                    thread1, NULL, NULL, NULL,
+                    THREAD_PRIORITY, 0, K_NO_WAIT);
+
+    k_thread_create(&thread2_data, thread2_stack, STACK_SIZE,
+                    thread2, NULL, NULL, NULL,
+                    THREAD_PRIORITY, 0, K_NO_WAIT);
 }
